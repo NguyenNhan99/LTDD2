dbba43682c7e5f1f9ea71f56c609675f007d2274
HuuThang1310 <buihuuthang199@gmail.com>
2019-05-29 13:16:11 +0700

    Add files via upload

---
+++ Tuan 1_7-5-2019/main/java/com/example/layouy_pj/GiaiThichSai.java
@@ -0,0 +1,32 @@
+package com.example.layouy_pj;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GiaiThichSai extends AppCompatActivity {
+
+    private Button quaytiep;
+    private TextView giaithichsai;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giai_thich_sai);
+        giaithichsai = (TextView) findViewById(R.id.giaithichsai);
+        Intent intent = getIntent();
+        String values = intent.getStringExtra("msg");
+        giaithichsai.setText(values);
+        quaytiep = (Button) findViewById(R.id.quaytiepnao);
+
+        quaytiep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(GiaiThichSai.this,ManHinhGameActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+}
